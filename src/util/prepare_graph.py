ed1cda85c1ec5638762fc5f7f0fdf5e31dbb5438
Eric Hein <ehein6@gatech.edu>
2015-11-19 18:53:36 -0500

    New script for splitting graphs into batches.

---
+++ src/util/prepare_graph.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def main():
+
+    # TODO
+    # Split input graph into 90% and 10% portions
+    # Pass 90% portion through converter
+    # Split 10% portion into chunks (leave in edge list format?)
+    pass
+
+if __name__ == "__main__":
+    main()
