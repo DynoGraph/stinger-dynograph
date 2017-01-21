d292be4c7ca2e9f3ab37887921ce0b63b5a353b7
Eric Hein <ehein6@gatech.edu>
2017-01-21 13:28:43 -0500

    Get new test from dynograph_util.

--- stinger_dynograph_test.cc
+++ stinger_dynograph_test.cc
@@ -2,3 +2,4 @@
 #include <dynograph_impl_test.h>
 
 INSTANTIATE_TYPED_TEST_CASE_P(STINGER_DYNOGRAPH, ImplTest, StingerServer);
+INSTANTIATE_TYPED_TEST_CASE_P(STINGER_DYNOGRAPH, CompareWithReferenceTest, StingerServer);
